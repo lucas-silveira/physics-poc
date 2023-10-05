41d873b5db1c6e83a55a9a3aad93890f2d1cdb75
Lucas Silveira <lugano1995@gmail.com>
2023-10-05 00:06:40 -0300

    improve basic physics

--- basic/main.cpp
+++ basic/main.cpp
@@ -4,59 +4,63 @@
 
 const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
-const sf::Vector2f GRAVITY = { 0.0f, 1000.0f };
+const sf::Vector2f GRAVITY = { 0.f, 1500.f };
 
 struct ball {
     float mass;
     sf::Vector2f velocity;
     sf::CircleShape shape;
 } ball;
-sf::RectangleShape floorLine;
 
-bool collision(sf::Vector2f circle, float radius, sf::Vector2f rect)
+sf::Vector2f calcNormalVelocity(sf::Vector2f currVel, float mass, float dt)
 {
-    float testX = circle.x;
-    float testY = circle.y;
-
-    if (circle.x < rect.x) testX = rect.x;
-    else if (circle.x > rect.x+SCREEN_WIDTH) testX = rect.x+SCREEN_WIDTH;
-
-    if (circle.y < rect.y) testY = rect.y;
-    else if (circle.y > rect.y+SCREEN_HEIGHT) testY = rect.y+SCREEN_HEIGHT;
-
-    float distX = circle.x-testX;
-    float distY = circle.y-testY;
-    float distance = sqrt( (distX*distX) + (distY*distY) );
-
-    if (distance <= radius)
-        return true;
-
-    return false;
+    sf::Vector2f newVelocity = {
+        currVel.x + (GRAVITY.x/mass) * dt,
+        currVel.y + (GRAVITY.y/mass) * dt
+    };
+    return newVelocity;
 }
 
-void applyPhysics(sf::Time elapsed)
+sf::Vector2f calcFrictionVelocity(sf::Vector2f currVel, float friction, float mass)
+{
+    sf::Vector2f newVelocity = {
+        (currVel.x*-friction)/mass,
+        (currVel.y*friction)/mass
+    };
+    return newVelocity;
+}
+
+sf::Vector2f calcPosition(sf::Vector2f currPos, sf::Vector2f currVel, float dt)
+{
+    sf::Vector2f newPos = {
+        currPos.x + currVel.x * dt,
+        currPos.y + currVel.y * dt
+    };
+    return newPos;
+}
+
+void runPhysics(sf::Time elapsed)
 {
     float dt = elapsed.asSeconds();
-    sf::Vector2f currVelocity = ball.velocity;
-    sf::Vector2f currPos = ball.shape.getPosition();
+    float radius = ball.shape.getRadius();
+    sf::Vector2f pos = ball.shape.getPosition();
 
-    sf::Vector2f newVelocity = {
-        currVelocity.x + (GRAVITY.x/ball.mass) * dt,
-        currVelocity.y + (GRAVITY.y/ball.mass) * dt,
-    };
-    sf::Vector2f newPos = {
-        currPos.x + newVelocity.x * dt,
-        currPos.y + newVelocity.y * dt,
-    };
+    sf::Vector2f newVelocity = calcNormalVelocity(ball.velocity, ball.mass, dt);
+    sf::Vector2f newPos = calcPosition(pos, newVelocity, dt);
 
-    if (collision(newPos, ball.shape.getRadius(), floorLine.getPosition()))
+    float cFriction = 0.9f;
+    float topDist = newPos.y; float bottomDist = SCREEN_HEIGHT-newPos.y;
+    float leftDist = newPos.x; float rightDist = SCREEN_WIDTH-newPos.x;
+
+    if (topDist <= radius || bottomDist <= radius)
     {
-        float cFriction = 0.9f;
-        newVelocity.x = (-currVelocity.x*cFriction)/ball.mass;
-        newVelocity.y = (-currVelocity.y*cFriction)/ball.mass;
-
-        newPos.x = currPos.x + newVelocity.x * dt;
-        newPos.y = currPos.y + newVelocity.y * dt;
+        newVelocity = calcFrictionVelocity(ball.velocity, -cFriction, ball.mass);
+        newPos = calcPosition(pos, newVelocity, dt);
+    }
+    if (leftDist <= radius || rightDist <= radius)
+    {
+        newVelocity = calcFrictionVelocity(ball.velocity, cFriction, ball.mass);
+        newPos = calcPosition(pos, newVelocity, dt);
     }
 
     ball.velocity = newVelocity;
@@ -65,16 +69,13 @@ void applyPhysics(sf::Time elapsed)
 
 void init()
 {
-    floorLine.setSize(sf::Vector2f((float)SCREEN_WIDTH, 5.0f ));
-    floorLine.setPosition(0, (float)SCREEN_HEIGHT-5.0f);
-
     sf::CircleShape ballShape;
-    ballShape.setRadius(50.0f);
-    ballShape.setOrigin(50.0f, 50.0f);
-    ballShape.setPosition(400.0f, 50.0f);
+    ballShape.setRadius(50.f);
+    ballShape.setOrigin(50.f, 50.f);
+    ballShape.setPosition(400.f, 50.f);
 
-    ball.mass = 1.1f;
-    ball.velocity = {0.0f, 0.0f};
+    ball.mass = 1.f;
+    ball.velocity = {0.f, 0.f};
     ball.shape = ballShape;
 }
 
@@ -97,10 +98,19 @@ int main()
                 window.close();
         }
 
-        applyPhysics(elapsed);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            float x;
+            if (sf::Mouse::getPosition(window).x <= SCREEN_WIDTH/2) x = -100.f;
+            else x = 100.f;
+
+            ball.velocity.x += x;
+            ball.velocity.y += -100.f;
+        }
+
+        runPhysics(elapsed);
  
         window.clear();
-        window.draw(floorLine);
         window.draw(ball.shape);
         window.display();
     }
